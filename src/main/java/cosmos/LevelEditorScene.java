777e99eeca5715d1f5742c49605ac7a52328612e
jeffersonrolino <jefferson.rolino@gmail.com>
2024-07-31 16:54:59 -0300

    GLSL Shaders

--- src/main/java/cosmos/LevelEditorScene.java
+++ src/main/java/cosmos/LevelEditorScene.java
@@ -3,6 +3,7 @@ package cosmos;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -110,6 +111,7 @@ public class LevelEditorScene extends Scene {
         defaultShader.use();
         defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        defaultShader.uploadFloat("uTime", Time.getTime());
 
         // Bind the VAO that we are using
         glBindVertexArray(vaoID);
